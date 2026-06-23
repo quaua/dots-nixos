0c21453d628787f01b92302d1764f39b9cbbe8f2
quaua <lolpopktk@gmail.com>
2026-06-24 02:31:31 +0500

    updated sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-cp -r ./config/* $HOME/.config/ && hyprctl reload
-sudo cp -r ./nixos/* /etc/nixos/ && sudo nixos-rebuild switch --flake /etc/nixos#reaper
+sudo rm -rf /etc/nixos/configuration.nix /etc/nixos/flake.nix /etc/nixos/home.nix && sudo cp -r ./nixos/* /etc/nixos/ && sudo nixos-rebuild switch --flake /etc/nixos#reaper
+rm -rf ~/.config/hypr/ ~/.config/quickshell/ && cp -r ./config/* ~/.config/
+
+awww img ~/nixos-config/wallpapers/ign_colorful.png
 matugen color hex "#be616a"
+hyprctl reload
